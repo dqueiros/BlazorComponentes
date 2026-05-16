3eaa8922fc10c3037c365a04f3254f54083a4c2d
dqueiros <diegoqueiros@hotmail.com>
2026-05-16 15:24:53 -0300

    Formulário com validação - ValidationSummary e ValidationMessage

--- BlazorFormularios/Entities/Aluno.cs
+++ BlazorFormularios/Entities/Aluno.cs
@@ -5,7 +5,7 @@ namespace BlazorFormularios.Entities
     public class Aluno
     {
         public int Id { get; set; }
-        [Required(ErrorMessage="o nome é obrigatório.")]
+        [Required(ErrorMessage="O nome é obrigatório.")]
         [StringLength(50,ErrorMessage="O nome deve ter no máximo 50 caracteres.")]
         public string Nome { get; set; } = string.Empty;
         [Required(ErrorMessage="O e-mail é obrigatório.")]
