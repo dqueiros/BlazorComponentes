396ae9d62d98f8f71c5fc5863075cd424f88d0ac
dqueiros <diegoqueiros@hotmail.com>
2026-05-16 14:31:17 -0300

    Criando seu primeiro formulário

---
+++ BlazorFormularios/Entities/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorFormularios.Entities
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Idade { get; set; }
+    }
+}
