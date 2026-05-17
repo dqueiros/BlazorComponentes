9355ee963649cec64168dc019a76abb92c6fd1d9
dqueiros <diegoqueiros@hotmail.com>
2026-05-17 14:12:13 -0300

    Formulário com Validação -  Limpando campos e Segurança

--- BlazorFormularios/Entities/Aluno.cs
+++ BlazorFormularios/Entities/Aluno.cs
@@ -11,7 +11,15 @@ namespace BlazorFormularios.Entities
         [Required(ErrorMessage="O e-mail é obrigatório.")]
         [EmailAddress(ErrorMessage="Informe um e-mail válido.")]
         public string Email { get; set; } = string.Empty;
-        [Range(1,120,ErrorMessage="A idade deve estar entre 1 e 120 anos.")]
+        [Range(1, 120, ErrorMessage = "A idade deve estar entre 1 e 120 anos.")]
         public int Idade { get; set; }
+        [Required(ErrorMessage = "O sexo é obrigatório.")]
+        [RegularExpression("M|F",ErrorMessage="Sexo deve ser 'M' ou 'F'.")]
+        public string Sexo { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A data da matrícula é obrigatório.")]
+        public DateTime Matricula { get; set; } = DateTime.Today;
+        [Range(0, 5000, ErrorMessage = "A mensalidade deve estar entre 0 e 5000.")]
+        public decimal Mensalidade { get; set; }
+        public bool Ativo { get; set; }
     }
 }
