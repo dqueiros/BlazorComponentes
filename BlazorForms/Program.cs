0cb2c2ab8e1913af0cac2b8d1eb1e0e84e378712
dqueiros <diegoqueiros@hotmail.com>
2026-05-17 15:12:40 -0300

    Navegação Aprimorada de formulários e Blazor Scaffolder

---
+++ BlazorForms/Program.cs
@@ -0,0 +1,50 @@
+﻿using BlazorForms.Components;
+using BlazorForms.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var connectionString = builder.Configuration
+                        .GetConnectionString("Sqlite");
+
+builder.Services.AddDbContextFactory<AppDbContext>(
+                    opt => opt.UseSqlite(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+CreateDatabase(app);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+static void CreateDatabase(WebApplication app)
+{
+    var serviceScope = app.Services.CreateScope();
+    var dataContext = serviceScope.ServiceProvider.GetService<AppDbContext>();
+    dataContext?.Database.EnsureCreated();
+}
